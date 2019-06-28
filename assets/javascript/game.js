67acfef2523f9add7caeab49c877f2690c8c469b
J-Palomino <JPalomino09@gmail.com>
2019-06-28 16:48:01 -0700

    renew game feature

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,64 +4,75 @@ var guessesLeft = 9;
 var guessedLetters = "";
 var newGuessedLetter = document.getElementById("newGuess").value;
 
-var mysteryLetter="";
+var mysteryLetter = "";
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- 
-startNewGame()
 
-function startNewGame(){
-    clicky();
-    letterPicker()
+startNewGame();
+
+function startNewGame() {
+  guessesLeft = 9;
+  guessedLetters = "";
+  clicky();
+  letterPicker();
 }
 
-function randomizer(){
-document.getElementById("mysteryLetter").innerHTML = mysteryLetter;
-};
+function reNewGame() {
+  guessesLeft = 9;
+  letterPicker();
+}
 
-function letterPicker(){
-    mysteryLetter=""
-    mysteryLetter += alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(mysteryLetter);
-    randomizer()
-};
+function randomizer() {
+  document.getElementById("mysteryLetter").innerHTML = mysteryLetter;
+}
 
-function clicky(){
-    document.getElementById("guessesLeft").innerHTML = guessesLeft;
-    clicky2();
-    clicky3();
-};
+function letterPicker() {
+  mysteryLetter = "";
+  mysteryLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  console.log(mysteryLetter);
+  randomizer();
+}
+
+function clicky() {
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  clicky2();
+  clicky3();
+}
 
 function clicky2() {
   document.getElementById("incorrectGuesses").innerHTML = losses;
-};
+}
 
 function clicky3() {
   document.getElementById("correctGuesses").innerHTML = wins;
-};
+}
 
 function makeGuess() {
-    event.preventDefault();
-    var newGuessedLetter = document.getElementById("newGuess").value;
-    if (newGuessedLetter !== mysteryLetter){
-        console.log(newGuessedLetter);
+  event.preventDefault();
+  var newGuessedLetter = document.getElementById("newGuess").value;
+  if (newGuessedLetter !== mysteryLetter) {
+    console.log(newGuessedLetter);
     guessedLetters += " " + newGuessedLetter;
     insertLetters();
-    document.getElementById("newGuess").value = " ";
-    losses++;
+    document.getElementById("newGuess").value = "";
+    newGuessedLetter = "";
     guessesLeft--;
     clicky()
+    if (guessesLeft < 1) {
+      console.log("gameover")
+      reNewGame();
     }
-    else{
-        document.getElementById("newGuess").value = " ";
-        event.preventDefault();
-        wins++;
-        guessesLeft--;
-        clicky();
-        letterPicker()
-    }
-};
+  } else {
+    document.getElementById("newGuess").value = "";
+    wins++;
+    console.log("winner winner")
+    guessesLeft = 9;
+    guessedLetters = "";
+    clicky();
+    letterPicker();
+  }
+}
 
 function insertLetters() {
-    event.preventDefault();
+  event.preventDefault();
   document.getElementById("lettersArr").innerHTML = guessedLetters;
-};
+}
